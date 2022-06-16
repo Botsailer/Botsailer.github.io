edfb341f07a347035db19665a12f07c03da4f1f2
Botsailer <95730735+Botsailer@users.noreply.github.com>
2022-06-17 00:03:42 +0530

    security update

---
+++ index.js
@@ -0,0 +1,38 @@
+function disableselect() {
+    return false
+}
+
+function reEnable() {
+    return false
+}
+
+window.onselectstart = new Function("return false")
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+}, false);
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+    }
+}
+document.querySelectorAll(".div2",()=>{
+    document.curs
+} )
